024d9a6ad55d7ca6cee154c00cdf98a5fe967a4b
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-16 13:55:21 +1200

    Update 01_get_budget_v1.py

--- 01_get_budget_v1.py
+++ 01_get_budget_v1.py
@@ -1,12 +1,12 @@
 """Get budget - Trial 1
-Uses "float" in the input to make sure that only numbers can be entered.
+Uses "float" in the input and only floats or integers are accepted.
 Loops until valid budget is entered.
 """
 budget = -1
 while budget < 0:
     budget = float(input("Enter budget: "))
     if budget < 0:  # if the number is negative than prints error
-        print("Please enter a positive number! Try again")
+        print("Please enter a positive number! Try again\n")
     else:
         break
 print("program continues...")
