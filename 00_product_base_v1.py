d151ef58634d684fc7fd98b40a1520271665aaee
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-16 13:32:34 +1200

    Create 00_product_base_v1.py

---
+++ 00_product_base_v1.py
@@ -0,0 +1,30 @@
+# Import statements
+
+# Functions go here
+
+# ******** Main Routine ********
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask user if they have used the program before and
+# show instructions if necessary
+
+# Repeat component 3-5 until "X" (loop)
+
+    # Get budget
+
+    # Get unit to use for products
+
+    # Get name, weight and price of product
+
+    # Calculate unit price
+
+    # Append details to list
+
+# Display all products details (within budget)
+
+# Display all products outside budget
+
+# Recommendation for "best buy"
+
+# Save data in a file
