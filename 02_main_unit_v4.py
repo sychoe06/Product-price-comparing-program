e54a59067fb1fb0d8b9b941b721da608470f4bc5
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-20 19:30:50 +1200

    component 2

--- 02_main_unit_v4.py
+++ 02_main_unit_v4.py
@@ -1,12 +1,12 @@
 """Get main unit for products - Trial 4
 Uses two parameters "question" and "valid_options" so that the function can be
 re-used more than once. So it doesn't only check units but could check for
-other options later in the program.
+other options later in the program. Changed function name to be more generic.
 """
 
 
 # Checks for valid options
-def check_units(question, valid_options):
+def check_valid(question, valid_options):
     response = input(question).lower()
     while response not in valid_options:
         print("Sorry that is not a valid choice\n")
@@ -16,6 +16,6 @@ def check_units(question, valid_options):
 
 # Main routine
 valid_units = ["kg", "mg", "g", "l", "kl", "ml"]
-main_unit = check_units("Enter main unit for products: ",
+main_unit = check_valid("Enter main unit for products: ",
                         valid_units)
 print("valid")  # prints "valid" for testing purposes
--- 02_main_unit_v5.py
+++ 02_main_unit_v5.py
@@ -8,19 +8,19 @@ input statement and changed the error message to being more generic
 
 
 # Checks for valid options
-def check_units(question, valid_options):
+def check_valid(question, valid_options):
     error = "Sorry that is not a valid choice\n"
     getting_option = True
     while getting_option is True:
         response = input(question).lower()
-        for unit in valid_options:
-            if response in unit:
-                response = unit[0].lower()
+        for option in valid_options:
+            if response in option:
+                response = option[0].lower()
                 return response
             else:
                 break
         print(error)
-    return check_units(question, valid_options)
+    return check_valid(question, valid_options)
 
 
 # Main routine
