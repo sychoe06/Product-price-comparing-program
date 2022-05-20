b429a234226d87f0e48fa928e850c94fb27b2391
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-20 19:50:49 +1200

    Update 03_product_details_v1.py

--- 03_product_details_v1.py
+++ 03_product_details_v1.py
@@ -1,4 +1,8 @@
 """Get product details - First test
+Used functions from 01_get_budget_v4 and 02_main_unit_v5 to help run my coding
+and to check for valid numbers and words.
+Uses a while loop to keep asking for product details until 'X' (which is the
+exit symbol) is entered instead of a product name.
 """
 
 
@@ -20,21 +24,35 @@ def number_checker(question):
 # Checks for valid options
 def check_valid(question, valid_options):
     error = "Sorry that is not a valid choice\n"
-    getting_option = True
-    while getting_option is True:
+    getting_option = "yes"
+    while getting_option == "yes":
         response = input(question).lower()
         for option in valid_options:
             if response in option:
                 response = option[0].lower()
                 return response
-            else:
-                break
+
         print(error)
-    return check_valid(question, valid_options)
+        return check_valid(question, valid_options)
 
 
 # Main routine
-print("Please enter the following details for the product!")
-name = input("Product name: ").title()
-weight = number_checker(f"Weight of {name} (without units): ")
-unit = check_valid("The unit for weight": )
+valid_units = [["kg", "kilograms", "kilogram"],
+               ["mg", "milligrams", "milligram"], ["g", "grams", "gram"],
+               ["l", "litre", "liter", "litres", "liters"],
+               ["kl", "kilolitre", "kiloliter", "kilolitres", "kiloliters"],
+               ["ml", "millilitre", "milliliter", "millilitres",
+                "milliliters"]]
+
+name = ""
+print("Please enter the following details for the product\n")
+while name != "X":
+    # Product details
+    name = input("Product name or 'X' to finish: ").title()
+    if name == "X":
+        break
+    else:
+        weight = number_checker(f"Weight of {name} (without units): ")
+        unit = check_valid("The unit for weight: ", valid_units)
+        price = number_checker("Price (without $ sign): ")
+        print()
