1ec5eb42d780b0d9780dce20744ea6cbeac336e0
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-20 18:23:07 +1200

    component 2

--- 02_main_unit_v4.py
+++ 02_main_unit_v4.py
@@ -1,8 +1,7 @@
 """Get main unit for products - Trial 4
 Uses two parameters "question" and "valid_options" so that the function can be
 re-used more than once. So it doesn't only check units but could check for
-other options later in the program. The error message is more generic now and
-made it more clear as to what kind of units to enter in the input statement
+other options later in the program. The error message is more generic now
 """
 
 
@@ -17,6 +16,6 @@ def check_units(question, valid_options):
 
 # Main routine
 valid_units = ["kg", "mg", "g", "l", "kl", "ml"]
-main_unit = check_units("Enter main unit for products (e.g. kg, ml etc): ",
+main_unit = check_units("Enter main unit for products: ",
                         valid_units)
 print("valid")  # prints "valid" for testing purposes
--- 02_main_unit_v5.py
+++ 02_main_unit_v5.py
@@ -2,20 +2,25 @@
 Made the list for "valid_units" more flexible by adding different variations
 of the units that way there is more than one way to enter a unit.
 Also made an "error" constant (in lowercase) for the error message.
+In order to prevent any errors I also put in examples of units to enter in the
+input statement.
 """
 
 
-# Checks for valid units
-def check_units(question, valid_choices):
+# Checks for valid options
+def check_units(question, valid_options):
     error = "Sorry that is not a valid choice\n"
-    response = input(question).lower()
-    for unit in valid_choices:
-        if response in unit:
-            response = unit[0].lower()
-            return response
-
-    print(error)
-    return check_units(question, valid_choices)
+    getting_option = True
+    while getting_option is True:
+        response = input(question).lower()
+        for unit in valid_options:
+            if response in unit:
+                response = unit[0].lower()
+                return response
+            else:
+                break
+        print(error)
+    return check_units(question, valid_options)
 
 
 # Main routine
@@ -25,9 +30,6 @@ valid_units = [["kg", "kilograms", "kilogram"],
                ["kl", "kilolitre", "kiloliter", "kilolitres", "kiloliters"],
                ["ml", "millilitre", "milliliter", "millilitres",
                 "milliliters"]]
-getting_unit = True
-while getting_unit is True:
-    main_unit = check_units("Enter main unit for products (e.g. kg, ml etc): ",
-                            valid_units)
-    getting_unit = False
+main_unit = check_units("Enter main unit for products (e.g. kg, ml etc): ",
+                        valid_units)
 print("valid")  # prints "valid" for testing purposes
