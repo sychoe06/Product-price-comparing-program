84a0e301b349d58ccb2ee78006b0a1ccaf574b75
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-23 14:08:42 +1200

    Update 00_product_base_v2.py

--- 00_product_base_v2.py
+++ 00_product_base_v2.py
@@ -19,6 +19,7 @@ def number_checker(question):
         except ValueError:
             print(error)
 
+
 # ******** Main Routine ********
 
 # Set up dictionaries / lists needed to hold data
@@ -26,18 +27,18 @@ def number_checker(question):
 # Ask user if they have used the program before and
 # show instructions if necessary
 
+# Get budget
+budget = number_checker("Enter budget: ")
+
+# Get main unit for products
+
+# Get product details (3)
+
+# Calculate unit price (4)
+
+# Append details to list (5)
+
 # Repeat component 3-5 until "X" (loop)
-
-    # Get budget
-    budget = number_checker("Enter budget: ")
-
-    # Get main unit for products
-
-    # Get product details
-
-    # Calculate unit price
-
-    # Append details to list
 
 # Display all products details (within budget)
 
