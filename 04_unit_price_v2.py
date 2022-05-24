53df4359f7c6986f6903eacea2d0667aff97e987
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-24 14:15:21 +1200

    component 4

--- 04_unit_price_v2.py
+++ 04_unit_price_v2.py
@@ -1,6 +1,6 @@
-"""Calculate unit price - trial 1
-I made a variable called "unit price" to calculate the unit price by dividing
-price by weight. I used "round" to round the unit price to 2dp.
+"""Calculate unit price - trial 2
+I used "%.2f" % unit price to make sure the unit price is rounded to 2dp and
+also adds a zero on the end if necessary.
 """
 # Using input statements for testing purposes only!
 weight = float(input("Enter weight: "))
@@ -8,5 +8,5 @@ unit = input("Enter unit: ")
 price = float(input("Enter price: "))
 
 # Calculates unit price
-unit_price = round(price / weight, 2)  # Rounds to 2dp
+unit_price = "%.2f" % (price / weight)  # Rounds to 2dp and adds 0 if necessary
 print(f"Unit price: ${unit_price} per {unit}")
--- 04_unit_price_v3.py
+++ 04_unit_price_v3.py
@@ -1,6 +1,5 @@
-"""Calculate unit price - trial 1
-I made a variable called "unit price" to calculate the unit price by dividing
-price by weight. I used "round" to round the unit price to 2dp.
+"""Calculate unit price - trial 3
+I used ".format()" to format my unit price to 2dp when I have calculated it.
 """
 # Using input statements for testing purposes only!
 weight = float(input("Enter weight: "))
@@ -8,5 +7,5 @@ unit = input("Enter unit: ")
 price = float(input("Enter price: "))
 
 # Calculates unit price
-unit_price = round(price / weight, 2)  # Rounds to 2dp
+unit_price = "{:.2f}".format(price / weight)  # Formats to 2dp
 print(f"Unit price: ${unit_price} per {unit}")
