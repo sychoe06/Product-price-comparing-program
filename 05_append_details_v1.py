a306e3e0c14c83fcb92cd688666c5b2d2b8013f8
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-26 23:46:30 +1200

    component 5

--- 05_append_details_v1.py
+++ 05_append_details_v1.py
@@ -2,6 +2,7 @@
 Checks whether the price that is entered for the product is less than or equal
 to the budget or above the budget.
 """
+# Temporary input statements - for testing purposes only
 budget = float(input("Budget: "))
 name = input("Name: ")
 price = float(input("Price: "))
--- 05_append_details_v2.py
+++ 05_append_details_v2.py
@@ -1,11 +1,30 @@
-"""Append details to list - testing 1
-Checks whether the price that is entered for the product is less than or equal
-to the budget or above the budget.
+"""Append details to list - based on 05_append_details_v1 (testing 2)
+Appends the details of one product into a list and then prints out list for
+testing purposes to show that it was successfully added in.
 """
+# Set up lists
+product_list = []
+unit_list = ["g", "kg", "mg", "L", "kl", "ml"]
+
+# Temporary input statements - for testing purposes only
 budget = float(input("Budget: "))
-name = input("Name: ")
+
+name = input("Product name: ")
+product_list.append(name)
+
+weight = float(input("Weight: "))
+product_list.append(weight)
+
+unit = input("Unit: ")
+if unit not in unit_list:
+    print("Please enter a valid unit!")
+    unit = input("Unit: ")
+product_list.append(unit)
+
 price = float(input("Price: "))
-if price <= budget:
-    print(f"{name} is within the budget")
-else:
-    print(f"{name} is above the budget")
+product_list.append(price)
+
+unit_price = "{:.2f}".format(price / weight)  # taken from 04_unit_price_v3
+product_list.append(unit_price)
+
+print(product_list)
