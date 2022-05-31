e4f37c4336c9649e2064ab12373191338d5225ab
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-05-31 14:20:38 +1200

    component 8

---
+++ 08_best_buy_v1.py
@@ -0,0 +1,9 @@
+"""Recommendation for "best buy" - testing 1
+"""
+# Test data in lists
+products_within_budget = [["Greggs", 0.04], ["Nescafe", 0.06]]
+products_above_budget = [["Moccona", 0.12]]
+
+test = [1, 1, 2, 3]
+
+print(min(test))
---
+++ 08_best_buy_v2.py
@@ -0,0 +1,55 @@
+"""Recommendation for "best buy" - testing 1
+"""
+import pandas
+
+
+# Currency formatting function for unit price only
+def currency(number):
+    return f"${number:,.2f} per {unit}"
+
+
+# Creates separate list for name and unit price for the products
+name_list = []
+unit_price_list = []
+
+# Put the separate lists above into a master list
+product_list = [name_list, unit_price_list]
+
+# Test data in lists
+products_within_budget = [["Greggs", 0.04], ["Nescafe", 0.06]]
+products_above_budget = [["Moccona", 0.12]]
+
+unit = "g"  # unit is for testing purposes only
+
+# Creates the products above budget dictionary with a label and then
+# a list for the product details like name and unit price
+products_above_dict = {
+    "Name": name_list,
+    "Unit Price": unit_price_list
+}
+
+# Adds the product details from list into the lists inside the dictionary
+for details in products_within_budget:
+    products_above_dict["Name"] += [details[0]]  # Adds name
+    products_above_dict["Unit Price"] += [details[1]]  # Adds unit price
+
+print(products_above_dict)  # Prints dictionary for testing purposes
+
+# Prints Product details in a well formatted way
+print("\n*** Products above the budget ***\n")
+product_frame = pandas.DataFrame(products_above_dict)  # creates data frame
+
+# Changes the index to reference the names rather than an actual index number
+product_frame = product_frame.set_index("Name")
+
+# Sorts the data frame in ascending order of the unit prices
+product_frame = product_frame.sort_values("Unit Price")
+
+# Formats the unit prices in currency and unit price format
+# so that they have $'s and the per unit (e.g. $0.25 per g)
+# "currency" is the call to the currency() function above
+product_frame["Unit Price"] = product_frame["Unit Price"].apply(currency)
+
+print(product_frame)  # Prints data frame
+
+print()
